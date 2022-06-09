7c9292e8c4b55c44500fc83d21cb09045a8cbb10
Badsalt <meldeg@edu.umea.se>
2022-06-09 15:40:01 +0200

    .

---
+++ docs/_app/pages/news.svelte-ec547f25.js
@@ -0,0 +1,51 @@
+import{S as re,i as ie,s as le,e as a,t as q,c as s,a as r,h as E,d as e,b as i,g as Lt,J as t,R as ne,W as ue,k as g,O as oe,m as w,Q as ce,_ as It,q as xt,o as Kt,p as de,a0 as me,N as Zt,T as at,U as fe,X as $t,Y as te,n as pe}from"../chunks/vendor-c6b62a7f.js";import{c as ee,i as st}from"../chunks/stores-173f9862.js";function ae(I){let c,n,u,o,p,h,d;return{c(){c=a("div"),n=a("div"),u=a("span"),o=q("The link is copied to the clipboard"),this.h()},l(m){c=s(m,"DIV",{class:!0});var y=r(c);n=s(y,"DIV",{});var v=r(n);u=s(v,"SPAN",{});var f=r(u);o=E(f,"The link is copied to the clipboard"),f.forEach(e),v.forEach(e),y.forEach(e),this.h()},h(){i(c,"class","alert alert-success shadow-lg absolute top-16 w-11/12 lg:w-3/4")},m(m,y){Lt(m,c,y),t(c,n),t(n,u),t(u,o),d=!0},p(m,y){},i(m){d||(ne(()=>{h&&h.end(1),p=fe(c,te,{delay:250,duration:1500,easing:$t}),p.start()}),d=!0)},o(m){p&&p.invalidate(),h=ue(c,te,{delay:250,duration:1e3,easing:$t}),d=!1},d(m){m&&e(c),m&&h&&h.end()}}}function ve(I){let c,n,u,o,p,h,d,m,y,v,f,St,rt,x,U,it,lt,J,S,nt,ut,M,b,C,ot,ct,W,dt,mt,O,ft,pt,Q,X,T,vt,ht,R,k,j,bt,_t,D,Y,V,gt,wt,G,N,qt,Et,B,K,P,kt,Z,yt,Tt,l=I[0]&&ae();return{c(){c=g(),n=a("div"),l&&l.c(),u=g(),o=a("ul"),p=a("li"),h=a("li"),d=a("article"),m=a("h1"),y=q("Suspendisse elit libero"),v=g(),f=a("iframe"),rt=g(),x=a("div"),U=a("div"),it=q("Published: 2 years ago"),lt=g(),J=a("div"),S=a("button"),nt=q("Share"),ut=g(),M=a("li"),b=a("article"),C=a("h1"),ot=q("Lorem ipsum dolor sit amet, consectetur adipiscing."),ct=g(),W=a("p"),dt=q(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut\r
+					accumsan quam erat, in hendrerit dolor luctus in. Morbi\r
+					semper magna id massa ullamcorper, a faucibus metus\r
+					porttitor. Proin ultrices posuere nisl, non egestas purus\r
+					vehicula mollis. Aliquam id leo sed metus gravida vulputate\r
+					eget nec ligula. Donec finibus, quam sit amet convallis\r
+					posuere, arcu quam rutrum enim, sed ullamcorper nibh sem sit\r
+					amet nibh. Quisque sapien ipsum, placerat in nisi a,\r
+					interdum tincidunt magna. Nullam aliquam eros id metus\r
+					pellentesque luctus. Donec porttitor eu leo eu scelerisque.\r
+					Aliquam aliquam nunc eu quam consectetur finibus. Vestibulum\r
+					condimentum neque et lacinia lobortis. Integer tempus auctor\r
+					nulla vitae fringilla. Vivamus sit amet ligula aliquet,\r
+					ultrices est vitae, interdum ipsum. Integer posuere diam sit\r
+					amet metus elementum sodales.`),mt=g(),O=a("p"),ft=q(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r
+					Morbi viverra dolor id erat aliquam, a mattis dolor\r
+					pulvinar. Etiam feugiat, urna sed gravida egestas, enim\r
+					sapien vestibulum quam, non facilisis purus urna sit amet\r
+					metus. Pellentesque habitant morbi tristique senectus et\r
+					netus et malesuada fames ac turpis egestas. Quisque\r
+					vulputate leo id massa accumsan feugiat. Sed lectus lacus,\r
+					facilisis eleifend felis eu, rhoncus commodo lectus. Nulla\r
+					facilisis, nulla nec luctus venenatis, arcu arcu semper\r
+					massa, vitae iaculis lorem orci non elit. Maecenas vitae\r
+					mauris in nisi aliquam convallis in vel sem. Etiam quis\r
+					massa velit.`),pt=g(),Q=a("div"),X=a("div"),T=a("button"),vt=q("Share"),ht=g(),R=a("li"),k=a("article"),j=a("h1"),bt=q("News from other Sources"),_t=g(),D=a("ul"),Y=a("li"),V=a("a"),gt=q("https://www.barncancerfonden.se/barncancerforskning/sa-ska-forskarna-utrota-barncancer/forskningsreportage/hostutlysningen---sophie-degerman/"),wt=g(),G=a("li"),N=a("a"),qt=q("https://www.vk.se/2018-12-21/spannande-att-kunna-paverka-framtidens-vard"),Et=g(),B=a("div"),K=a("div"),P=a("button"),kt=q("Share"),this.h()},l(_){oe('[data-svelte="svelte-1sl63pr"]',document.head).forEach(e),c=w(_),n=s(_,"DIV",{class:!0});var $=r(n);l&&l.l($),u=w($),o=s($,"UL",{class:!0});var A=r(o);p=s(A,"LI",{});var se=r(p);se.forEach(e),h=s(A,"LI",{});var Dt=r(h);d=s(Dt,"ARTICLE",{id:!0,class:!0});var F=r(d);m=s(F,"H1",{class:!0});var Vt=r(m);y=E(Vt,"Suspendisse elit libero"),Vt.forEach(e),v=w(F),f=s(F,"IFRAME",{title:!0,class:!0,src:!0,frameborder:!0,allow:!0}),r(f).forEach(e),rt=w(F),x=s(F,"DIV",{class:!0});var tt=r(x);U=s(tt,"DIV",{class:!0});var Nt=r(U);it=E(Nt,"Published: 2 years ago"),Nt.forEach(e),lt=w(tt),J=s(tt,"DIV",{});var Pt=r(J);S=s(Pt,"BUTTON",{class:!0});var At=r(S);nt=E(At,"Share"),At.forEach(e),Pt.forEach(e),tt.forEach(e),F.forEach(e),Dt.forEach(e),ut=w(A),M=s(A,"LI",{class:!0});var Ut=r(M);b=s(Ut,"ARTICLE",{id:!0,class:!0});var L=r(b);C=s(L,"H1",{class:!0});var Mt=r(C);ot=E(Mt,"Lorem ipsum dolor sit amet, consectetur adipiscing."),Mt.forEach(e),ct=w(L),W=s(L,"P",{});var Ct=r(W);dt=E(Ct,`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut\r
+					accumsan quam erat, in hendrerit dolor luctus in. Morbi\r
+					semper magna id massa ullamcorper, a faucibus metus\r
+					porttitor. Proin ultrices posuere nisl, non egestas purus\r
+					vehicula mollis. Aliquam id leo sed metus gravida vulputate\r
+					eget nec ligula. Donec finibus, quam sit amet convallis\r
+					posuere, arcu quam rutrum enim, sed ullamcorper nibh sem sit\r
+					amet nibh. Quisque sapien ipsum, placerat in nisi a,\r
+					interdum tincidunt magna. Nullam aliquam eros id metus\r
+					pellentesque luctus. Donec porttitor eu leo eu scelerisque.\r
+					Aliquam aliquam nunc eu quam consectetur finibus. Vestibulum\r
+					condimentum neque et lacinia lobortis. Integer tempus auctor\r
+					nulla vitae fringilla. Vivamus sit amet ligula aliquet,\r
+					ultrices est vitae, interdum ipsum. Integer posuere diam sit\r
+					amet metus elementum sodales.`),Ct.forEach(e),mt=w(L),O=s(L,"P",{class:!0});var Ot=r(O);ft=E(Ot,`Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r
+					Morbi viverra dolor id erat aliquam, a mattis dolor\r
+					pulvinar. Etiam feugiat, urna sed gravida egestas, enim\r
+					sapien vestibulum quam, non facilisis purus urna sit amet\r
+					metus. Pellentesque habitant morbi tristique senectus et\r
+					netus et malesuada fames ac turpis egestas. Quisque\r
+					vulputate leo id massa accumsan feugiat. Sed lectus lacus,\r
+					facilisis eleifend felis eu, rhoncus commodo lectus. Nulla\r
+					facilisis, nulla nec luctus venenatis, arcu arcu semper\r
+					massa, vitae iaculis lorem orci non elit. Maecenas vitae\r
+					mauris in nisi aliquam convallis in vel sem. Etiam quis\r
+					massa velit.`),Ot.forEach(e),pt=w(L),Q=s(L,"DIV",{class:!0});var Qt=r(Q);X=s(Qt,"DIV",{});var Rt=r(X);T=s(Rt,"BUTTON",{class:!0});var jt=r(T);vt=E(jt,"Share"),jt.forEach(e),Rt.forEach(e),Qt.forEach(e),L.forEach(e),Ut.forEach(e),ht=w(A),R=s(A,"LI",{class:!0});var Bt=r(R);k=s(Bt,"ARTICLE",{id:!0,class:!0});var z=r(k);j=s(z,"H1",{class:!0});var Ht=r(j);bt=E(Ht,"News from other Sources"),Ht.forEach(e),_t=w(z),D=s(z,"UL",{});var et=r(D);Y=s(et,"LI",{});var Ft=r(Y);V=s(Ft,"A",{class:!0,href:!0});var zt=r(V);gt=E(zt,"https://www.barncancerfonden.se/barncancerforskning/sa-ska-forskarna-utrota-barncancer/forskningsreportage/hostutlysningen---sophie-degerman/"),zt.forEach(e),Ft.forEach(e),wt=w(et),G=s(et,"LI",{});var Jt=r(G);N=s(Jt,"A",{class:!0,href:!0});var Wt=r(N);qt=E(Wt,"https://www.vk.se/2018-12-21/spannande-att-kunna-paverka-framtidens-vard"),Wt.forEach(e),Jt.forEach(e),et.forEach(e),Et=w(z),B=s(z,"DIV",{class:!0});var Xt=r(B);K=s(Xt,"DIV",{});var Yt=r(K);P=s(Yt,"BUTTON",{class:!0});var Gt=r(P);kt=E(Gt,"Share"),Gt.forEach(e),Yt.forEach(e),Xt.forEach(e),z.forEach(e),Bt.forEach(e),A.forEach(e),$.forEach(e),this.h()},h(){document.title="News",i(m,"class","font-bold"),i(f,"title","video"),i(f,"class","mx-auto mt-10 w-1/2 aspect-video"),ce(f.src,St="https://player.vimeo.com/video/370874522?h=bd3326f2b6&color=ffffff&title=0&byline=0&portrait=0")||i(f,"src",St),i(f,"frameborder","0"),i(f,"allow","autoplay; fullscreen; picture-in-picture"),f.allowFullscreen=!0,i(U,"class",""),i(S,"class","btn btn-primary"),i(x,"class","flex px-5 flex-row justify-between bg-base-300"),i(d,"id","1"),i(d,"class","border-2 rounded-xl border-black"),i(C,"class","font-bold"),i(O,"class","mt-5"),i(T,"class","btn btn-primary"),i(Q,"class","flex px-5 flex-row justify-end bg-base-300"),i(b,"id","2"),i(b,"class","border-2 rounded-xl border-black"),i(M,"class","mt-5"),i(j,"class","font-bold"),i(V,"class","underline decoration-blue-600"),i(V,"href","https://www.barncancerfonden.se/barncancerforskning/sa-ska-forskarna-utrota-barncancer/forskningsreportage/hostutlysningen---sophie-degerman/"),i(N,"class","underline decoration-blue-600"),i(N,"href","https://www.vk.se/2018-12-21/spannande-att-kunna-paverka-framtidens-vard"),i(P,"class","btn btn-primary"),i(B,"class","flex px-5 flex-row justify-end bg-base-300"),i(k,"id","3"),i(k,"class","border-2 rounded-xl border-black"),i(R,"class","mt-5"),i(o,"class","my-12"),i(n,"class","container w-11/12 lg:w-3/4 text-center mb-40 svelte-1i9sh8z")},m(_,H){Lt(_,c,H),Lt(_,n,H),l&&l.m(n,null),t(n,u),t(n,o),t(o,p),t(o,h),t(h,d),t(d,m),t(m,y),t(d,v),t(d,f),t(d,rt),t(d,x),t(x,U),t(U,it),t(x,lt),t(x,J),t(J,S),t(S,nt),t(o,ut),t(o,M),t(M,b),t(b,C),t(C,ot),t(b,ct),t(b,W),t(W,dt),t(b,mt),t(b,O),t(O,ft),t(b,pt),t(b,Q),t(Q,X),t(X,T),t(T,vt),t(o,ht),t(o,R),t(R,k),t(k,j),t(j,bt),t(k,_t),t(k,D),t(D,Y),t(Y,V),t(V,gt),t(D,wt),t(D,G),t(G,N),t(N,qt),t(k,Et),t(k,B),t(B,K),t(K,P),t(P,kt),Z=!0,yt||(Tt=[It(S,"click",I[2]),It(T,"click",I[3]),It(P,"click",I[4])],yt=!0)},p(_,[H]){_[0]?l?(l.p(_,H),H&1&&xt(l,1)):(l=ae(),l.c(),xt(l,1),l.m(n,u)):l&&(pe(),Kt(l,1,1,()=>{l=null}),de())},i(_){Z||(xt(l),Z=!0)},o(_){Kt(l),Z=!1},d(_){_&&e(c),_&&e(n),l&&l.d(),yt=!1,me(Tt)}}}function he(I,c,n){let u,o;Zt(I,st,v=>n(0,u=v)),Zt(I,ee,v=>n(5,o=v)),at(ee,o="news",o),at(st,u=!1,u);function p(v){h();var f=window.location.host+window.location.pathname+"#"+v;navigator.clipboard.writeText(f)}async function h(){at(st,u=!0,u),await new Promise(v=>setTimeout(v,2e3)),at(st,u=!1,u)}return[u,p,()=>p(1),()=>p(2),()=>p(3)]}class ge extends re{constructor(c){super();ie(this,c,he,ve,le,{})}}export{ge as default};
